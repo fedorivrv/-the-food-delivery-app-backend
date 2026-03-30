dc0e93dae4b53fb6afaadcaf6a26dcfad6bda0e0
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2026-03-30 18:16:53 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: false,
+    credentials: true,
   })
 );
 
