312dd448e38d58116d8d8764ba352ab750019e93
Roman Fedoriv <fedoriv.roman.v@gmail.com>
2026-03-30 18:19:58 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -26,9 +26,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'https://the-food-delivery-app-frontend-n05rjvet2.vercel.app',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    ],   
     credentials: true,
   })
 );
